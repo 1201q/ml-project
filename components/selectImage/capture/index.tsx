a89a40deab9dfb7537ce7a2dd50519bf2c806a8a
hwang <1201q@naver.com>
2024-05-13 14:41:43 +0900

    [fix] 카메라 고침

--- components/selectImage/capture/index.tsx
+++ components/selectImage/capture/index.tsx
@@ -15,11 +15,6 @@ const CaptureImagePage = () => {
           <Webcam
             mirrored={true}
             videoConstraints={{ ...size, facingMode: "user" }}
-            style={{
-              position: "absolute",
-              width: "100%",
-              height: "100%",
-            }}
           />
         )}
       </CameraContainer>
@@ -42,16 +37,20 @@ const Container = styled(motion.div)`
 `;
 
 const CameraContainer = styled.div`
-  height: 65%;
+  height: calc(100% - 150px);
   position: relative;
 `;
 const ControllerContainer = styled.div`
-  height: 35%;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 200px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   color: white;
+  background-color: black;
 `;
 
 const CaptureButton = styled(motion.button)`
@@ -69,19 +68,6 @@ const CaptureBtnContainer = styled.div`
   height: 50px;
   background-color: gray;
   border-radius: 50%;
-`;
-
-const CameraBox = styled.canvas`
-  border: 1px solid rgba(255, 255, 255, 0.7);
-
-  width: calc(100% - 40px);
-  height: calc(100% - 40px);
-  position: absolute;
-  top: 50%;
-  right: 50%;
-  transform: translateX(50%) translateY(-50%);
-  z-index: 100;
-  background-color: transparent;
 `;
 
 export default CaptureImagePage;
