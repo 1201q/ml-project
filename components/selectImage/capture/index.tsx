855017d5daaf36092d5a84553580b3e1c21fce54
hwang <1201q@naver.com>
2024-05-13 14:32:19 +0900

    [fix] 박스 치움

--- components/selectImage/capture/index.tsx
+++ components/selectImage/capture/index.tsx
@@ -19,12 +19,9 @@ const CaptureImagePage = () => {
               position: "absolute",
               width: "100%",
               height: "100%",
-              objectFit: "cover",
             }}
           />
         )}
-
-        <CameraBox />
       </CameraContainer>
       <ControllerContainer>
         {size?.width} /{size?.height} / {size?.aspectRatio}
