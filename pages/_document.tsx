e63883cab72d4ea180915643b909d62087ee8efc
hwang <1201q@naver.com>
2024-06-08 17:37:26 +0900

    [feat] ico 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,6 +39,12 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
+          {/* <link
+            rel="stylesheet"
+            as="style"
+            crossOrigin="anonymous"
+            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
+          /> */}
           <link
             rel="stylesheet"
             as="style"
